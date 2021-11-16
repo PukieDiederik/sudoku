af868ac9080a97cc15ba10b5caf295b50a00082a
pukediederik <diederik.boeren@gmail.com>
2021-11-16 16:13:54 +0000

    Changed how it outputs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.IO;
-using System.Diagnostics; //for stopwatch
+using System.Text;
 using System.Collections.Generic;
 
 using CommandLine;
@@ -52,12 +52,7 @@ namespace programming
                 byte[] unsolvedSudoku = sudokus[i]; //SHOULDN'T BE MODIFIED
                 byte[] workingSudoku = new byte[81]; //the sudoku that can be modified
                 Array.Copy(unsolvedSudoku, 0, workingSudoku, 0, unsolvedSudoku.Length); //copy the sudoku to the work-sudoku
-
-                //write input to console
-                Console.Write("Input:  ");
-                foreach(byte b in unsolvedSudoku) {Console.Write(b); } Console.Write("\n");
-                
-                
+   
                 //start solving the sudoku
                 int curPos = 0; //the current position the solver is working on
                 bool direction = true; //true forward, false backward
@@ -68,19 +63,17 @@ namespace programming
                     else { //if it doesnt need to skip numbers
                         direction = true; //make sure we are moving forward again
                         workingSudoku[curPos]++; //start checking the next number
-                        while(!isValid(workingSudoku, curPos) && workingSudoku[curPos] < 9){
+                        while(!IsValid(workingSudoku, curPos) && workingSudoku[curPos] < 9){
                             workingSudoku[curPos]++; //keep increasing until we find a valid number or > 9
                         }
                         //finalize numbers
                         if(workingSudoku[curPos] < 9) curPos++;
-                        else if(workingSudoku[curPos] == 9 && isValid(workingSudoku,curPos)) curPos++;
+                        else if(workingSudoku[curPos] == 9 && IsValid(workingSudoku,curPos)) curPos++;
                         else { workingSudoku[curPos] = 0; curPos--; direction = false; }
                     }
                 }
                 //write the solution in consolse
-                Console.Write("output: ");
-                foreach(byte b in workingSudoku) { Console.Write(b); } Console.Write("\n"); 
-
+                Console.WriteLine($"[{i}]: {SudokuToString(unsolvedSudoku)} {SudokuToString(workingSudoku)}");
             }
         }
 
@@ -95,8 +88,15 @@ namespace programming
             }
             return parsedSudoku;
         }
+
+        //expects to get byte[81] as input
+        static string SudokuToString(byte[] b){
+            StringBuilder sb = new StringBuilder(81);
+            for(int i = 0; i < 81; i++) sb.Append(b[i]);
+            return sb.ToString();
+        }
     
-        static bool isValid (byte[] sudoku, int pos){
+        static bool IsValid (byte[] sudoku, int pos){
             byte num = sudoku[pos];
             //check if the number is too high
             if(num > 9) return false;
